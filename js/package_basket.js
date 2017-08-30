fa4c53de58aef225aaf15bb8c20c8fa1a19ff8e2
b00303037 <b00303037@gmail.com>
2017-08-30 19:41:03 +0800

    Cookie 4

--- js/package_basket.js
+++ js/package_basket.js
@@ -59,7 +59,7 @@ function addToBasket(id) {
 		}
 	}
 
-	setHer(cookie, 7)
+	setCookie(cookie, 7)
 	loadCookieToBasket()
 }
 
@@ -90,10 +90,219 @@ function removeFromBasket(id) {
 		}
 	}
 
-	setHer(cookie, 7)
+	setCookie(cookie, 7)
 	loadCookieToBasket()
 }
 
 function loadCookieToBasket() {
 	console.log('loadCookieToBasket()')
+
+	if(cookie.hasOwnProperty('card')) {
+		const cardId = cookie.card
+
+		getProductDetail(cardId)
+		.then((cardData) => {
+			renderBasketCardBoxView(cardData)
+		})
+
+	} else {
+		$('#Basket_card_box').html(cardBoxEmpty)
+	}
+
+	if(cookie.hasOwnProperty('font')) {
+		const fontId = cookie.font
+
+		getProductDetail(fontId)
+		.then((fontData) => {
+			renderBasketFontBoxView(fontData)
+		})
+
+	} else {
+		$('#Basket_font_box').html(fontBoxEmpty)
+	}
+
+	if(cookie.hasOwnProperty('gifts')) {
+		const gifts = cookie.gifts
+
+		const giftsData = Object.keys(gifts).reduce((acc, giftId) => {
+
+			getProductDetail(giftId)
+			.then((result) => {
+
+				const giftData = {
+					quantity: gifts[giftId],
+					...result
+				}
+
+				acc.push(giftData)
+			})
+
+			return acc
+		}, [])
+
+		renderBasketGiftBoxView(giftsData)
+
+	} else {
+		$('#Basket_gift_box').html(giftBoxEmpty)
+	}
+}
+
+const cardBoxEmpty = `
+<div class='block220'>
+	<img
+		src='../images/cover_card.png'
+		width='220'
+		height='220' />
+</div>
+<div class='block60 flex_center bgc_white'>
+	<img
+		src='../images/title_card.png'
+		width='40'
+		height='40'
+		class='cursor'
+		onclick='setCategory("card")' />
+</div>
+<div class='block40 flex_spacebetween'>
+	<div class='rec_text_40 chi_20'>CARD</div>
+</div>
+`
+
+const fontBoxEmpty = `
+<div class='block220'>
+	<img
+		src='../images/cover_font.png'
+		width='220'
+		height='220' />
+</div>
+<div class='block60 flex_center bgc_white'>
+	<img
+		src='../images/title_font.png'
+		width='40'
+		height='40'
+		class='curosr'
+		onclick='setCategory("font")' />
+</div>
+<div class='block40 flex_spacebetween'>
+	<div class='rec_text_40 chi_20'>FONT</div>
+</div>
+`
+
+const cardOrFontBoxContent = (data) => `
+	<div class='square220'>
+		<img src='${data.cover}'
+			 width='220'
+			 height='220' />
+		<button
+			type='button'
+			class='button_circle bgi_remove_from_basket'
+			onclick='removeFromBasket("${data.id}")'></button>
+	</div>
+	<div class='block60 flex_left bgc_white'>
+		<div class='rec_text_60 chi_16_30'>${data.name}</div>
+	</div>
+	<div class='block40 flex_right'>
+		<div class='rec_text_40 chi_20'>$ ${data.price}</div>
+	</div>
+`
+
+const giftBoxEmpty = `
+<div class='block220'>
+	<img src='../images/cover_gift.png'
+		 width='580'
+		 height='220' />
+</div>
+<div class='block60 flex_center bgc_white'>
+	<img src='../images/title_gift.png'
+		 width='40'
+		 height='40'
+		 class='cursor'
+		 onclick='setCategory(gift)' />
+</div>
+<div class='block40 flex_spacebetween'>
+	<div class='rec_text_40 chi_20'>GIFT</div>
+</div>
+`
+
+const getGiftsDataHTML = (giftsData) => {
+	return giftsData.reduce((acc, gift) => acc += `
+		<div class='rec60 flex_left'>
+			<div class='square60'>
+				<img src=''
+					 width='60'
+					 height='60' />
+				<button
+					type='button'
+					class='button_circle bgi_remove_from_basket'
+					onclick='removeFromBasket("${gift.id}")'></button>
+			</div>
+			<div class='box220'>
+				<div class='rec_text_60 chi_16_30'>${gift.name}</div>
+			</div>
+			<div class='box90 flex_center'>
+				<div class='rec_text_40'>$ ${gift.price}</div>
+			</div>
+			<div class='box90 flex_center'>
+				<button
+					type='button'
+					class='square20 border bgi_minus'
+					onclick='adjustGiftQuantity("${gift.id}", -1)'></button>
+				<input class='rec_digit border chi_16_20 bgc_white'
+					   value='${gift.quantity}'
+					   disabled />
+				<button
+					type='button'
+					class='square20 border bgi_plus'
+					onclick='adjustGiftQuantity("${gift.id}", 1)'></button>
+			</div>
+			<div class='box90 flex_center'>
+				<div class='rec_text_40'>$ ${gift.price * gift.quantity}</div>
+			</div>
+		</div>
+	`, '')
+}
+
+const giftBoxContent = (giftsData) => `
+	<div class='block280'>
+		<div class='block40 flex_left bgc_lightgray'>
+			<div class='box280 text_align'>商品名稱</div>
+			<div class='box90 text_align'>單價</div>
+			<div class='box90 text_align'>數量</div>
+			<div class='box90 text_align'>小計</div>
+		</div>
+		<div class='block240 flex_block overflow_y bgc_white'>
+
+			${getGiftsDataHTML(giftsData)}
+
+		</div>
+	</div>
+	<div class='block40 flex_spacebetween'>
+		<div class='rec_text_40 chi_20'>GIFT</div>
+		<div class='rec_text_40 chi_20'>$ ${calculateGiftsSum(giftsData)}</div>
+	</div>
+`
+
+function getProductDetail(id) {
+
+}
+
+function calculateGiftsSum(giftsData) {
+	return giftsData.reduce((acc, gift) => {
+		acc += gift.price * gift.quantity
+		return acc
+	}, 0)
+}
+
+function adjustGiftQuantity(id, n) {
+	console.log('adjustQuantity: '+ id +', n:' + n)
+
+	if(n===1 && cookie.gifts[id] < 6) {
+		cookie.gifts[id] += 1
+	}
+
+	if(n===-1 && cookie.gifts[id] > 1) {
+		cookie.gifts[id] -= 1
+	}
+
+	setCookie(cookie, 7)
+	loadCookieToBasket()
 }
