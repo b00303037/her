3630953683473983715b6210f6c5fb30cb50851c
b00303037 <b00303037@gmail.com>
2017-08-30 19:12:44 +0800

    Cookie2

--- js/package_basket.js
+++ js/package_basket.js
@@ -28,7 +28,72 @@ function setCookie(cookie, exdays) {
   document.cookie = "her=" + JSON.stringify(cookie) + ";" + expires + ";path=/";
 }
 
+function addToBasket(id) {
+	console.log('addToBasket(' + id + ')')
+	const prefix = id.slice(0, 1).toLowerCase()
 
-function addToBasket() {
+	let type
+	switch(prefix) {
+		case 'c':
+			type = 'card'
+			break;
+		case 'f':
+			type = 'font'
+			break;
+		case 'g':
+			type = 'gift'
+			break;
+	}
 
+	if(type === 'card' || type === 'font') {
+		herObject[type] = id
+	} else if(type === 'gift') {
+		if(herObject.hasOwnProperty('gifts')) {
+			herObject.gifts[id] =  1
+		} else {
+			herObject = {
+				gifts: {
+					[id]: 1
+				}
+			}
+		}
+	}
+
+	setHer(herObject, 7)
+	loadCookieToBasket()
+}
+
+function removeFromBasket(id) {
+	console.log('removeFromBasket(' + id + ')')
+	const prefix = id.slice(0, 1).toLowerCase()
+
+	let type
+	switch(prefix) {
+		case 'c':
+			type = 'card'
+			break;
+		case 'f':
+			type = 'font'
+			break;
+		case 'g':
+			type = 'gift'
+			break;
+	}
+
+	if(type === 'card' || type === 'font') {
+		delete herObject[type]
+	} else if(type === 'gift') {
+		console.log('delete: '+ id)
+		delete herObject.gifts[id]
+		if($.isEmptyObject(herObject.gifts)) {
+			delete herObject.gifts
+		}
+	}
+
+	setHer(herObject, 7)
+	loadCookieToBasket()
+}
+
+function loadCookieToBasket() {
+	console.log('loadCookieToBasket()')
 }
